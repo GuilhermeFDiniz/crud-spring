f9491e488c38c223005273a0512885d3ff49bc78
GuilhermeDiniz <guidinizeng@gmail.com>
2023-01-14 17:56:44 -0300

    Spring Rest and Request Mapping for Controller

--- src/main/java/br/com/gps/guilherme/controller/CompanyController.java
+++ src/main/java/br/com/gps/guilherme/controller/CompanyController.java
@@ -1,10 +1,15 @@
 package br.com.gps.guilherme.controller;
 
 import br.com.gps.guilherme.controller.DTO.CompanyDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.Arrays;
 import java.util.List;
 
-
+@RestController
+@RequestMapping("company") //localhost:8080/company/
 public class CompanyController {
 
 }
