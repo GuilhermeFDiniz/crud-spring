c8a0b9802cc624ddabe591a95141cbbf99373af2
GuilhermeDiniz <guidinizeng@gmail.com>
2023-01-14 17:31:29 -0300

    Spring Initializer

---
+++ src/main/java/br/com/gps/guilherme/GuilhermeApplication.java
@@ -0,0 +1,13 @@
+package br.com.gps.guilherme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuilhermeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuilhermeApplication.class, args);
+	}
+
+}
